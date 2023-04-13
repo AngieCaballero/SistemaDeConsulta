ceb80082bc24c4d43a042dbd32ad626763b170dd
AngieCaballero <karma2000angiejaz@gmail.com>
2023-04-12 20:58:33 -0600

    Update proyect

--- app/Http/Controllers/DatosController.php
+++ app/Http/Controllers/DatosController.php
@@ -13,7 +13,7 @@ class DatosController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'http://3401-165-98-36-14.ngrok.io/estudiantes/obtener_estudiantes_anio',
+            CURLOPT_URL => 'http://apiacademica.loc/estudiantes/obtener_estudiantes_anio',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -22,7 +22,7 @@ class DatosController extends Controller
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGlhY2FkZW1pY2EudW5hbmxlb24uZWR1Lm5pIiwiZXhwIjoxNjUxMTY1OTE2LCJhdWQiOiJ1bmFubGVvbi5lZHUubmkiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6W119.MXQuLeyTz7crzCWb9qHzTj6-zykTXim8ua7MX61NUm4'
+                'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGlhY2FkZW1pY2EudW5hbmxlb24uZWR1Lm5pIiwiZXhwIjoxNjUxMjcwNzE3LCJhdWQiOiJ1bmFubGVvbi5lZHUubmkiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6W119.4xxFRSTz-RqTZiHK1RI7YOackC1qnk_yASP8mXbcOi4'
             ),
         ));
 
@@ -32,14 +32,14 @@ class DatosController extends Controller
 
         $response = curl_exec($curl);
         //dd(json_decode($response)->error);
-        var_dump($response);
-        die();
-        $respuesta=json_decode($response);
+        //var_dump($response);
+        //die();
+        //$respuesta=json_decode($response);
 
 
         curl_close($curl);
 
-        return $respuesta;
+        return $response;
     }
 
     public function getPostgrados ()
